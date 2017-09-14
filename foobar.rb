0679951f2b7879a7f0deb51c667b7962647a0d86
Yudi Tan <yudi98@berkeley.edu>
2017-09-13 20:48:46 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,8 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map(&:to_i).map{|e| e + 2}.delete_if{|e| e % 2 != 0}.uniq.delete_if{|e| e > 10}.sum
+
   end
 end
 
